8153edd809dddec8e64abb84be9daa40ff96a1d9
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-02-23 17:28:13 -0500

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,9 @@
+#Write a program to calculate the total of a grocery list. 
+#Create 5 variables to represent the items in the list and their prices. 
+#Add up all the prices and print out the entire shopping list and total.
+#Your output should look like this:
+#    Reciept:
+#    item 1: Eggs - 3.99
+#    item 2: Milk - 2.50
+#    ....
+#    Total:        10.65
