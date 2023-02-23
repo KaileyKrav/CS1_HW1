d1b5fa7b3dad80abdb6483c841dcf5f171364d4e
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-02-23 17:29:26 -0500

    Create Question1.py

---
+++ Question1.py
@@ -0,0 +1,5 @@
+#Write a program that asks the user for their first and last name and their birthday. 
+#Calculate their current age from their birthday and print out the following:
+#Full Name:_______
+#Birthday: _______
+#Age: _______
